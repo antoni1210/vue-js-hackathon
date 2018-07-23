d87ac7df3b4eb86da0ed5d029560830d09bbe32f
Martin Allgood <martintallgood@gmail.com>
2018-07-23 11:10:45 +0100

    Rendered initial page

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+
+// Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+import Home from './components/Home';
+
+const routes = [
+  { path: '/', component: Home }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app');
