543583eb21acff7f80efaae0ad3871960f24fbd0
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-07-23 12:35:14 +0100

    started to include the googleplace api

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <template>
   <div id="app">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" v-bind:class="{ 'is-active': navIsOpen }">
+        <div class="navbar-end">
+          <router-link to="/" class="navbar-item">Home</router-link>
+        </div>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
-}
+  name: 'app',
+  data() {
+    return { navIsOpen: false };
+  },
+  methods: {
+    toggleNav() {
+      this.navIsOpen = !this.navIsOpen;
+    }
+  },
+  watch: {
+    '$route'() {
+      this.navIsOpen = false;
+    }
+  }
+};
 </script>
 
 <style></style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,37 @@
+
 <template>
-  <h1>Hello World</h1>
+  <section>
+    <h1>Hello World</h1>
+    <ul>
+      <li v-for="todo in todos">{{ todo.text }}</li>
+    </ul>
+
+  </section>
 </template>
 
 <script>
+import axios from 'axios';
+
   export default {
-  name: 'Home'
+  name: 'Home',
+  data() {
+    return {
+      venues: [],
+      todos: [
+        { text: 'Learning JavaScript'},
+      ]
+    }
+  },
+
+  mounted() {
+    axios
+      .get('https://maps.googleapis.com/maps/api/place/radarsearch/application/json?location=51.503186,-0.126446&radius=5000&type=movie_theatre&key=AIzaSyD5l8LZyITAru4A3U8nyx-riblGU0yt6Cw')
+      .then(res => {
+        this.data.venues = res.results;
+        console.log(res);
+      });
+
+  }
   };
 </script>
 
