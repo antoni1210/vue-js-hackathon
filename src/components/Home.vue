4373b901d9ecc485787e92641ede51be4907b2fd
Martin Allgood <martintallgood@gmail.com>
2018-07-23 16:45:14 +0100

    Removed workings out to get axios request to work. Pivoting to try and find a different api instead so we dont need to make a proxy request

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,7 +12,7 @@
 <script>
 import axios from 'axios';
 
-  export default {
+export default {
   name: 'Home',
   data() {
     return {
@@ -24,20 +24,13 @@ import axios from 'axios';
   },
 
   mounted() {
-    const config = {
-      headers: { API_KEY: 'AIzaSyDdsDypPEp9X7T8qg-p41_pz_c2EYsqr8Y' }
-    }
-    axios.get('https://maps.googleapis.com/maps/api/place/radarsearch/application/json?location=51.503186,-0.126446&radius=5000&type=movie_theatre', config)
 
-  //   axios({
-  //     method: 'GET',
-  //     url: 'https://maps.googleapis.com/maps/api/place/radarsearch/application/json?location=51.503186,-0.126446&radius=5000&type=movie_theatre',
-  // })
-  //   .then(res => {
-  //   console.log(res.data);
-  // });
-    }
-  };
+    axios.get('https://restcountries.eu/rest/v2/all')
+      .then(res => console.log(res.data))
+
+
+  }
+};
 </script>
 
 <style></style>
