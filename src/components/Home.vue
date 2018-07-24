a63fc093391ac8a3c0b30f710a2d65b40b06b12c
Martin Allgood <martintallgood@gmail.com>
2018-07-24 11:08:39 +0100

    Got the returned list of venues rendered on the page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,9 @@
 
 <template>
   <section>
-    <h1>Hello World</h1>
+    <ul>
+      <li v-for="venue in venues">{{ venue.name }}</li>
+    </ul>
   </section>
 </template>
 
@@ -19,12 +21,9 @@ export default {
   },
 
   mounted() {
-    axios({
-      method: 'GET',
-      url: `https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d17f941735&ll=51.5153,0.0725&client_id=QXJWRY5OPPFQ1RKWHMPWPFV3OSS5MQBFU0KNH22WOJASLAOT&client_secret=RMBIJZ3JWOO0DRXLSIWN5I21UX5V5TAZQBXB3CBUYVRTG3GJ&v=20180724`
-    })
+    axios.get(`https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d17f941735&ll=51.5153,0.0725&client_id=QXJWRY5OPPFQ1RKWHMPWPFV3OSS5MQBFU0KNH22WOJASLAOT&client_secret=RMBIJZ3JWOO0DRXLSIWN5I21UX5V5TAZQBXB3CBUYVRTG3GJ&v=20180724`)
       .then(res => {
-
+        this.venues = res.data.response.venues;
         console.log(res.data.response.venues)
       });
   }
