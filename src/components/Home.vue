f62686346f7c0934bc3967d16f9052202372a810
Antoni Alston <sntonialston@gmail.com>
2018-07-24 13:28:21 +0100

    resolved some conflicts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,9 @@
 <template>
-<<<<<<< HEAD
   <section class="section">
     <div class="container">
       <form  v-on:submit.prevent="handleSubmit()">
         <div class="field">
-          <label class="label">Search</label>
+          <label class="label">search</label>
           <div class="control">
             <Autocomplete name="Search" v-bind:handle-place-change="handlePlaceChange"/>
           </div>
@@ -14,12 +13,6 @@
         </div>
       </form>
     </div>
-=======
-  <section>
-    <ul>
-      <li v-for="venue in venues">{{ venue.name }}</li>
-    </ul>
->>>>>>> development
   </section>
 </template>
 
@@ -42,20 +35,18 @@ export default {
       console.log(this.venue);
     }
   },
-<<<<<<< HEAD
   components: {
     Autocomplete
-=======
-
+  },
   mounted() {
     axios.get(`https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d17f941735&ll=51.5153,0.0725&client_id=QXJWRY5OPPFQ1RKWHMPWPFV3OSS5MQBFU0KNH22WOJASLAOT&client_secret=RMBIJZ3JWOO0DRXLSIWN5I21UX5V5TAZQBXB3CBUYVRTG3GJ&v=20180724`)
       .then(res => {
         this.venues = res.data.response.venues;
         console.log(res.data.response.venues)
       });
->>>>>>> development
   }
 };
+
 
 </script>
 
